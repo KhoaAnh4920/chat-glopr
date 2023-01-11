caa547e8beeb587fd522dcc56262b0a33fd5c663
khoaanh4920 <dh51800321@student.stu.edu.vn>
2023-01-11 17:43:51 +0700

    update config redis

--- src/shared/cache/redis.ts
+++ src/shared/cache/redis.ts
@@ -2,9 +2,9 @@ import Redis from 'ioredis';
 
 console.log('Check: ', process.env.REDIS_HOST);
 export const redisClient = new Redis({
-  host: 'redis-19300.c295.ap-southeast-1-1.ec2.cloud.redislabs.com',
-  port: 19300,
-  password: 'c1X5kKMlrRWK8b5vJv6WDLqSrYmtatQZ',
+  host: 'containers-us-west-141.railway.app',
+  port: 6288,
+  password: 'l8l0w0sjzEqmLQw5vleR',
 });
 
 // export const redisClient = new Redis(6379);
