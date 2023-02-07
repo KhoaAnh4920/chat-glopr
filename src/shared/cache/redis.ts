c4d29da1655ce662c427bf5341789ba120486803
khoaanh4920 <dh51800321@student.stu.edu.vn>
2023-02-07 21:42:40 +0700

    update config redis

--- src/shared/cache/redis.ts
+++ src/shared/cache/redis.ts
@@ -1,9 +1,9 @@
 import Redis from 'ioredis';
 
-// export const redisClient = new Redis({
-//   host: process.env.REDIS_HOST,
-//   port: +process.env.REDIS_PORT,
-//   password: process.env.REDIS_PASSWORD,
-// });
+export const redisClient = new Redis({
+  host: process.env.REDIS_HOST,
+  port: +process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
 
-export const redisClient = new Redis({ host: 'redis' });
+// export const redisClient = new Redis({ host: 'redis' });
